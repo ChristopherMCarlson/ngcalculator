1655c46e2e99514bcd3a0524a33c7e3fe282c356
Christopher Carlson <c.m.carlson13@gmail.com>
2019-11-24 01:05:53 -0700

    not sure how to get repeat to work...

--- src/app/store-front/store-front.component.ts
+++ src/app/store-front/store-front.component.ts
@@ -7,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreFrontComponent implements OnInit {
 
+  cart = [];
+  Guitars = [
+    {
+      Name: 'Gibson SG Standard',
+      Price: '1499.00',
+      Image: '../../assets/GibsonSG.jpg'
+    },
+    {
+      Name: 'Gibson Firebird',
+      Price: '1999.00',
+      Image: '../../assets/GibsonFirebird.jpg'
+    },
+    {
+      Name: 'B C Rich Mockingbird',
+      Price: '1499.00',
+      Image: '../../assets/BCRichMockingbird.jpg'
+    },
+    {
+      Name: 'Dean Cadi',
+      Price: '849.00',
+      Image: '../../assets/DeanCadi.jpg'
+    },
+    {
+      Name: 'Ibanez Iceman',
+      Price: '899.00',
+      Image: '../../assets/IbanezIceman.jpg'
+    },
+    {
+      Name: 'ESP Snakebyte',
+      Price: '1249.00',
+      Image: '../../assets/ESPSnakebyte.jpg'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
