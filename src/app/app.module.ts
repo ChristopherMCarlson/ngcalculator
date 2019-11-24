4303b67471a221ac0b5d16881778ec4670a4eea1
Christopher Carlson <c.m.carlson13@gmail.com>
2019-11-24 14:47:33 -0700

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { StoreFrontComponent } from './store-front/store-front.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -14,9 +20,15 @@ import { StoreFrontComponent } from './store-front/store-front.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/store-front/store-front.component.ts
+++ src/app/store-front/store-front.component.ts
@@ -12,34 +12,42 @@ export class StoreFrontComponent implements OnInit {
     {
       Name: 'Gibson SG Standard',
       Price: '1499.00',
+      Id: 'guitSPHVP',
       Image: '../../assets/GibsonSG.jpg'
     },
     {
       Name: 'Gibson Firebird',
       Price: '1999.00',
+      Id: 'guitISZNN',
       Image: '../../assets/GibsonFirebird.jpg'
     },
     {
       Name: 'B C Rich Mockingbird',
       Price: '1499.00',
+      Id: 'guitE5N3C',
       Image: '../../assets/BCRichMockingbird.jpg'
     },
     {
       Name: 'Dean Cadi',
       Price: '849.00',
+      Id: 'guitM9LBE',
       Image: '../../assets/DeanCadi.jpg'
     },
     {
       Name: 'Ibanez Iceman',
       Price: '899.00',
+      Id: 'guitWOMIU',
       Image: '../../assets/IbanezIceman.jpg'
     },
     {
       Name: 'ESP Snakebyte',
       Price: '1249.00',
+      Id: 'guitS8N48',
       Image: '../../assets/ESPSnakebyte.jpg'
     },
   ]
+  activeProduct = this.Guitars
+
 
   constructor() { }
 
